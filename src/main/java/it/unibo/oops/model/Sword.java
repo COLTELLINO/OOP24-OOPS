a71230fe9e8598e94ea27bdfb700f479e23f5ea0
Patrignani Filippo <filippo.patrignani2@studio.unibo.it>
2025-05-06 14:15:11 +0200

    sword directions

--- src/main/java/it/unibo/oops/model/Sword.java
+++ src/main/java/it/unibo/oops/model/Sword.java
@@ -11,24 +11,20 @@ import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 justification = "To position the weapon, the player size and position are needed, "
         + "and while it's not necessary for the player to be externally mutable for this class, it has to be for others.")
 public class Sword extends Weapon {
-    //NOTA: DURATION COOLDOWN e TIMER DOVREBBERO POTER USUFRUIRE DELLA CLASSE TIMER CREANDO UN 
-    //TIMERIMPL(60) (con 60 == 1 secondo a 60 FPS) - Java-The-Cup
     private static final int WIDTH = 15;
     private static final int HEIGHT = 45;
     private static final double DURATION = 30;
     private static final double COOLDOWN = 60;
-
-    private double timer;
+    private double duration;
     private double cooldown;
     private boolean active;
     private final Player player;
+    private boolean lastSwingRight = true;
     /**
      * @param player
      */
     public Sword(final Player player) {
         this.active = false;
-        this.timer = DURATION;
-        this.cooldown = COOLDOWN;
         this.player = player;
     }
     /**
@@ -37,11 +33,11 @@ public class Sword extends Weapon {
     @Override
     public void update() {
         if (active) {
-            timer--;
-            if (timer <= 0) {
+            duration--;
+            if (duration <= 0) {
                 active = false;
                 this.cooldown = COOLDOWN;
-                this.timer = DURATION;
+                this.duration = DURATION;
             }
         } else {
             if (cooldown <= 0) {
@@ -50,7 +46,7 @@ public class Sword extends Weapon {
                 cooldown--;
             }
         }
-    }
+    }    
     /**
      * Draws the sword.
      * @param g
@@ -61,26 +57,35 @@ public class Sword extends Weapon {
             g.setColor(Color.BLUE);
             int drawX = player.getX(), drawY = player.getY();
             int swordWidth = WIDTH, swordHeight = HEIGHT; 
-            switch (player.getDirection()) {
-                case Direction.UP:
-                    drawX = player.getX() + player.getSize() / 2 - WIDTH / 2;
-                    drawY = player.getY() - HEIGHT;
-                    break;
+
+            Direction currentDirection = player.getDirection();
+
+            if (currentDirection == Direction.NONE || currentDirection == Direction.UP || currentDirection == Direction.DOWN) {
+                if (lastSwingRight) {
+                    currentDirection = Direction.RIGHT;
+                } else {
+                    currentDirection = Direction.LEFT;
+                }
+            } else if (currentDirection == Direction.UPRIGHT || currentDirection == Direction.DOWNRIGHT) {
+                currentDirection = Direction.RIGHT;
+            } else if (currentDirection == Direction.UPLEFT || currentDirection == Direction.DOWNLEFT) {
+                currentDirection = Direction.LEFT;
+            }
+
+            switch (currentDirection) {
                 case Direction.RIGHT:
                     drawX = player.getX() + player.getSize();
                     drawY = player.getY() + player.getSize() / 2 - WIDTH / 2;
                     swordWidth = HEIGHT; 
                     swordHeight = WIDTH;
-                    break;
-                case Direction.DOWN:
-                    drawX = player.getX() + player.getSize() / 2 - WIDTH / 2;
-                    drawY = player.getY() + player.getSize();
+                    lastSwingRight = true;
                     break;
                 case Direction.LEFT:
                     drawX = player.getX() - HEIGHT;
                     drawY = player.getY() + player.getSize() / 2 - WIDTH / 2;
                     swordWidth = HEIGHT;
                     swordHeight = WIDTH;
+                    lastSwingRight = false;
                     break;
                 default:
                     break;
