d57c88856cf181cbc49e33db27e40dc51ab4bab7
GiovanniPerreon <giovanni.perreon@studio.unibo.it>
2025-06-22 13:21:11 +0200

    Updated relazioneOOP

--- src/test/java/it/unibo/oop/junit/TestEnemyFactory.java
+++ src/test/java/it/unibo/oop/junit/TestEnemyFactory.java
@@ -6,11 +6,14 @@ import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import it.unibo.oop.model.entities.Bat;
 import it.unibo.oop.model.entities.BossEnemy;
+import it.unibo.oop.model.entities.Cultist;
 import it.unibo.oop.model.entities.Ghost;
 import it.unibo.oop.model.entities.Player;
 import it.unibo.oop.model.entities.Skull;
 import it.unibo.oop.model.entities.Slime;
+import it.unibo.oop.model.entities.Zombie;
 import it.unibo.oop.model.factories.EnemyFactory;
 import it.unibo.oop.model.factories.EnemyFactoryImpl;
 /**
@@ -34,6 +37,9 @@ class TestEnemyFactory {
     void testEnemyFactory() {
         assertInstanceOf(Slime.class, enemyFactory.createBaseSlime(0, 0, player));
         assertInstanceOf(Ghost.class, enemyFactory.createBaseGhost(0, 0, player));
+        assertInstanceOf(Zombie.class, enemyFactory.createBaseZombie(0, 0, player));
+        assertInstanceOf(Bat.class, enemyFactory.createBaseBat(0, 0, player));
+        assertInstanceOf(Cultist.class, enemyFactory.createBaseCultist(0, 0, player));
         assertInstanceOf(Skull.class, enemyFactory.createBaseSkull(0, 0, player));
         assertInstanceOf(BossEnemy.class, enemyFactory.createBoss(enemyFactory.createBaseSlime(0, 0, player)));
         assertFalse(enemyFactory.createBaseGhost(0, 0, player) instanceof Slime);
